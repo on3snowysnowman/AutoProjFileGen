3c6510efc8f2e186d667e6725b406bd850e49c1f
Joel <on3snowysnowman@gmail.com>
2024-08-12 21:32:17 -0400

    Added source files in for open source reading.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ generate.cpp FileSystemHandler.o TextFileHandler.o JsonHandler.o TimeObserver.o -o generate.exe
+g++ generate.cpp FileSystemHandler.o TextFileHandler.o JsonHandler.o TimeObserver.o -Iinclude -o generate.exe
