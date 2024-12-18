85f7355ca939d44a751a959bcbd48fcbfafbcdfc
Joel Height <on3snowysnowman@gmail.com>
2024-12-17 22:38:12 -0500

    Added a build file for windows.

--- build.bat
+++ build.bat
@@ -1,1 +1,17 @@
-g++ -c generate.cpp src/FileSystemHandler.cpp src/JsonHandler.cpp src/TextFileHandler.cpp src/TimeObserver.cpp -Iinclude && g++ generate.o FileSystemHandler.o JsonHandler.o TextFileHandler.o TimeObserver.o -o generate.exe
+@REM g++ -c generate.cpp src/FileSystemHandler.cpp src/JsonHandler.cpp src/TextFileHandler.cpp src/TimeObserver.cpp -Iinclude && g++ generate.o FileSystemHandler.o JsonHandler.o TextFileHandler.o TimeObserver.o -o generate.exe
+@echo off
+
+
+if not exist "obj" (
+    mkdir obj
+)
+
+cd obj
+
+g++ -c ../src/FileSystemHandler.cpp ../src/JsonHandler.cpp ../src/TextFileHandler.cpp ../src/TimeObserver.cpp ../generate.cpp -I../include
+
+cd ..
+
+g++ obj/FileSystemHandler.o obj/JsonHandler.o obj/TextFileHandler.o obj/TimeObserver.o obj/generate.o -o generate
+
+rmdir /s /q "obj"
